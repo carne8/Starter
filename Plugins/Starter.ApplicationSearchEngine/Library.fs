d4a17899ca4a5402be12bab0c5d4869e2185aaf9
Gaël Bouquain <gael.bouquain@gmail.com>
2025-03-04 23:35:07 +0100

    Add frequency sorting in application search engine
    Sort applications from the most used to the least used

--- Plugins/Starter.ApplicationSearchEngine/Library.fs
+++ Plugins/Starter.ApplicationSearchEngine/Library.fs
@@ -1,8 +1,6 @@
 ﻿namespace Starter.ApplicationSearchEngine
 
 open System
-open System.IO
-open System.Xml
 open System.Diagnostics
 open System.Threading.Tasks
 open System.Collections.Generic
@@ -16,153 +14,15 @@ open Starter.SearchEngine
 open Avalonia.Media.Imaging
 open IconHelper
 
-[<AutoOpen>]
-module Helpers =
-    module Option =
-        let require f v =
-            match v |> f with
-            | true -> Some v
-            | false -> None
-
-    module Dict =
-        let tryGet key (d: IDictionary<_, _>) =
-            match d.TryGetValue key with
-            | true, v -> Some v
-            | false, _ -> None
-
-module ShellItem =
-    module Property =
-        let get propertyName (shellItem: ShellItem) =
-            propertyName
-            |> Ole32.PROPERTYKEY
-            |> shellItem.Properties.GetPropertyString
-            |> Option.require (String.IsNullOrEmpty >> not)
-
-        let getInstallPath (shellItem: ShellItem) =
-            shellItem
-            |> get "System.AppUserModel.PackageInstallPath"
-            |> Option.orElseWith (fun _ ->
-                shellItem
-                |> get "System.Link.TargetParsingPath"
-                |> Option.map Path.GetDirectoryName
-            )
-
-type Package =
-    { Id: string * string
-      ManifestPath: string }
-module Package =
-    let ofShellItem (shellItem: ShellItem) =
-        option {
-            let! packageId = shellItem |> ShellItem.Property.get "System.AppUserModel.ID"
-            let! installPath = shellItem |> ShellItem.Property.getInstallPath
-            let manifestPath = Path.Combine(installPath, "AppxManifest.xml")
-
-            // Check if file exists
-            do! manifestPath
-                |> File.Exists
-                |> Option.require id
-                |> Option.ignore
-
-            return { Id = packageId.Split("!") |> fun arr -> arr[0], arr[1]
-                     ManifestPath = manifestPath }
-        }
-
-    let getIconResourceName (package: Package) =
-        let xml = XmlDocument()
-        xml.Load package.ManifestPath
-
-        let nsManager = XmlNamespaceManager xml.NameTable
-        nsManager.AddNamespace("default", "http://schemas.microsoft.com/appx/manifest/foundation/windows10")
-        nsManager.AddNamespace("uap", "http://schemas.microsoft.com/appx/manifest/uap/windows10")
-
-        let selector = $"""/default:Package/default:Applications/default:Application[@Id="{package.Id |> snd}"]/uap:VisualElements/@Square44x44Logo"""
-
-        xml.SelectSingleNode(selector, nsManager)
-        |> Option.ofNull
-        |> Option.map _.Value
-
-    /// Retrieve the files corresponding to the resource
-    let getResourceFiles (resource: string) package =
-        let packageFolder = package.ManifestPath |> Path.GetDirectoryName
-        let resourceFolder = resource |> Path.GetDirectoryName
-        let absolutePath = Path.Combine(packageFolder, resourceFolder)
-
-        let resourceName = Path.GetFileNameWithoutExtension resource
-        let resourceExt = Path.GetExtension resource
-
-        Directory.GetFiles(absolutePath, $"{resourceName}*{resourceExt}")
-        |> Array.map (fun file ->
-            let fileName = Path.GetFileNameWithoutExtension file
-
-            if fileName = resourceName then // Is no qualifiers
-                file, dict []
-            else
-                // Parse qualifiers (ex: fileName = logo.contrast-high_scale-400)
-                let qualifiers =
-                    fileName.Split(".")
-                    |> Array.last
-                    |> _.Split("_")
-                    |> Array.map (fun qualifier ->
-                        if qualifier.Contains "-" then
-                            let arr = qualifier.Split "-"
-                            arr[0], arr[1]
-                        else
-                            qualifier, ""
-                    )
-                    |> dict
-
-                file, qualifiers
-        )
-
-    let getIcon package = option {
-        let! resourceName = package |> getIconResourceName
-        let resourceFiles = package |> getResourceFiles resourceName
-
-        return!
-            resourceFiles
-            |> Array.sortBy (fun (_file, qualifiers) ->
-                let contrastScore =
-                    match qualifiers |> Dict.tryGet "contrast" with
-                    | None
-                    | Some "standard" -> 0
-                    | _ -> 100
-
-                let targetSizeScore =
-                    match qualifiers |> Dict.tryGet "targetsize" with
-                    | None -> 44 // Because Square44x44Logo
-                    | Some size ->
-                        match Int32.TryParse size with
-                        | true, scale -> scale
-                        | false, _ -> Int32.MaxValue
-                    |> fun scale -> 100 - scale |> abs
-
-                let scaleScore =
-                    match qualifiers |> Dict.tryGet "scale" with
-                    | None -> 100
-                    | Some scale ->
-                        match Int32.TryParse scale with
-                        | true, scale -> scale
-                        | false, _ -> Int32.MaxValue
-                    |> fun scale -> 200 - scale |> abs // Distance to 200
-
-                let qualifiersCountScore = qualifiers.Count
-
-                contrastScore*100_000
-                + targetSizeScore*10_000
-                + scaleScore*100
-                + qualifiersCountScore
-            )
-            |> Array.tryHead
-            |> Option.map fst
-    }
-
 [<RequireQualifiedAccess>]
 type ExecutionPath =
     | ExeFile of string
     | PackageId of string
 
-type SearchResult =
-    { Name: string
+type Application =
+    { Id: string
+      Name: string
+      Score: (int * DateTimeOffset) option
       ExecutionPath: ExecutionPath
       LoadIcon: unit -> Task<Bitmap> }
 
@@ -171,20 +31,21 @@ type SearchResult =
         member this.LoadIcon() = this.LoadIcon()
 
 /// Indexes apps and cache their icon
+/// App score based on https://github.com/ajeetdsouza/zoxide/wiki/Algorithm#frecency
 type AppIndexer() =
-    let applications = List<SearchResult>()
+    let applications = List<Application>()
     let mutable indexingFinished = false
 
     let getAppIcon (targetPathOpt: string option) (app: ShellItem) =
         // Icon for the Appx apps
         let packageIconOpt =
             app
-            |> Package.ofShellItem
-            |> Option.bind Package.getIcon
+            |> WindowsPackage.ofShellItem
+            |> Option.bind WindowsPackage.getIcon
 
         let getShellIcon () =
             app.Images
-               .GetImage(SIZE(35, 35),  ShellItemGetImageOptions.ResizeToFit) // TODO: Pass the size of the icon from the host program
+               .GetImage(SIZE(35, 35),  ShellItemGetImageOptions.IconOnly) // TODO: Pass the size of the icon from the host program
                .ToAvaloniaBitmap()
 
         match packageIconOpt, targetPathOpt with
@@ -212,23 +73,60 @@ type AppIndexer() =
                     | None, None -> None
 
                 app.Dispose()
-                applications.Add(
-                    { Name = name
-                      ExecutionPath = executionPath
-                      LoadIcon = fun () -> Task.singleton icon }
-                )
+
+                { Id = app.ParsingName
+                  Name = name
+                  Score = None
+                  ExecutionPath = executionPath
+                  LoadIcon = fun () -> Task.singleton icon }
+                |> applications.Add
             } |> ignore
 
-        applications.Sort(fun x y -> x.Name.CompareTo y.Name)
+        applications.Sort(fun app1 app2 -> app1.Name.CompareTo app2.Name)
         indexingFinished <- true
     )
 
     do indexApps() |> ignore
 
-    member _.Applications =
+    member _.FindApp(query: string) =
         match indexingFinished with
-        | true -> Some applications
-        | false -> None
+        | false -> Seq.empty
+        | true ->
+            applications
+            |> Seq.choose (fun app ->
+                match app.Name.Contains(query, StringComparison.OrdinalIgnoreCase) with
+                | true -> Some app
+                | false -> None
+            )
+            |> Seq.sortBy (fun app ->
+                match app.Score with
+                | None -> 0., app.Name.ToLowerInvariant(), TimeSpan.MaxValue
+                | Some (score, lastAccessDate) ->
+                    let d = DateTimeOffset.Now - lastAccessDate
+                    let s = float -score
+
+                    let f =
+                        if d.TotalHours < 1 then s * 4.
+                        elif d.TotalDays < 1 then s * 2.
+                        elif d.TotalDays < 7 then s / 2.
+                        else s / 4.
+                    f, app.Name.ToLowerInvariant(), d
+            )
+            :?> ISearchResult seq
+
+    member _.IncreaseAppScore(app: Application) =
+        let i = applications.IndexOf app
+
+        let newScore =
+            app.Score
+            |> Option.map (fun (s, _) -> s+1, DateTimeOffset.Now)
+            |> Option.defaultValue (1, DateTimeOffset.Now)
+
+        let newApp = { app with Score = Some newScore }
+
+        applications.RemoveAt i
+        applications.Insert(i, newApp)
+
 
 type ApplicationSearchEngine() =
     let indexer = AppIndexer()
@@ -238,20 +136,15 @@ type ApplicationSearchEngine() =
         member _.DisplayName = "Application"
 
         member _.Search(_ct, query) =
-            match indexer.Applications with
-            | None -> Observable.empty
-            | Some apps ->
-                apps
-                |> Seq.choose (fun app ->
-                    match app.Name.Contains(query, StringComparison.OrdinalIgnoreCase) with
-                    | true -> app :> ISearchResult |> Some
-                    | false -> None
-                )
-                |> Observable.single
+            query
+            |> indexer.FindApp
+            |> Observable.single
 
         member _.SearchResultSelected(searchResult) =
             match searchResult with
-            | :? SearchResult as sr ->
+            | :? Application as sr ->
+                indexer.IncreaseAppScore sr
+
                 let execStr =
                     match sr.ExecutionPath with
                     | ExecutionPath.ExeFile path -> path
