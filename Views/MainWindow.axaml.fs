c8ded59bf3b47594e2c28820b46c14593ee58865
Gaël Bouquain <gael.bouquain@gmail.com>
2025-05-20 12:36:23 +0200

    Fix backspace

--- Views/MainWindow.axaml.fs
+++ Views/MainWindow.axaml.fs
@@ -128,7 +128,7 @@ type MainWindow() as this =
         let d = System.EventHandler<KeyEventArgs>(fun _ e ->
             match e.PhysicalKey with
             | PhysicalKey.Backspace ->
-                if this.ViewModel.SingleSearchEngineMode.Value.IsSome then
+                if this.ViewModel.SingleSearchEngineMode.Value.IsSome && this.TextBox.Text = "" then
                     this.ViewModel.ResetSingleSearchEngineMode()
                     e.Handled <- true
 
