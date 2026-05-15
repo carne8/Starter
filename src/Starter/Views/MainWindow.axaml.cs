01d3f61df018a4bc54f74a75e45d0f8426827536
Gaël Bouquain <gael.bouquain@gmail.com>
2026-05-15 19:03:54 +0200

    Change window position

--- src/Starter/Views/MainWindow.axaml.cs
+++ src/Starter/Views/MainWindow.axaml.cs
@@ -76,7 +76,7 @@ public partial class MainWindow : TranslucentWindow
             screen.WorkingArea.TopLeft
             + new PixelPoint(
                 (int)Math.Round((screen.WorkingArea.Width - Width * screen.Scaling) / 2.0),
-                (int)Math.Round(screen.WorkingArea.Height * (5.0 / 16.0))
+                (int)Math.Round((screen.WorkingArea.Height - 470 * screen.Scaling) / 2.0)
             );
 
         // Reset focus
