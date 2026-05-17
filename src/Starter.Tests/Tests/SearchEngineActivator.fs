191867d22a9f43383866a1fdbb2b6327bc4a58b5
Gaël Bouquain <gael.bouquain@gmail.com>
2026-05-17 22:21:26 +0200

    Add a test

--- src/Starter.Tests/Tests/SearchEngineActivator.fs
+++ src/Starter.Tests/Tests/SearchEngineActivator.fs
@@ -58,4 +58,60 @@ let testActivatorPrefix () =
         Assert.AreNotEqual(activators[0].Name, label.Text, "Displayed text should be the activator name")
     )
 
-// TODO: Check that activator is passed to dynamic search engine
+[<AvaloniaTest>]
+let ensureActivatorIsPassedToDynamicSearchEngine () =
+    let activators =
+        [ Mock.activator "activator-id" "engine-1"
+          Mock.activator "other-activator-id" "engine-2" ]
+
+    let calls = ResizeArray()
+
+    let engines: ISearchEngine list =
+        [ Mock.dynamicSearchEngine "engine-1" [ activators[0] ] false (fun _ _ activator -> calls.Add activator; [])
+          Mock.dynamicSearchEngine "engine-2" [ activators[1] ] false (fun _ _ _ -> []) ]
+
+    let config =
+        { Configuration.Default with
+            ActivatorPrefixes =
+                activators
+                |> List.map (fun a -> a.Id, a.SearchEngineId)
+                |> Map.ofList }
+
+    Mock.withWindowConfig config engines (fun window _ ->
+        // Type text
+        Dispatcher.UIThread.RunJobs() // Let window acknowledge about vm
+        window.Show()
+        Dispatcher.UIThread.RunJobs() // Let textbox grab focus
+        window.KeyTextInput activators[0].SearchEngineId
+
+        Assert.AreEqual(
+            activators[0],
+            calls |> Seq.last,
+            "Dynamic search should have been called with the activator as parameter"
+        )
+
+        // Remove activator
+        window.KeyPress(Key.Back, RawInputModifiers.Control, PhysicalKey.Backspace, null)
+        window.KeyRelease(Key.Back, RawInputModifiers.Control, PhysicalKey.Backspace, null)
+        window.KeyPress(Key.Back, RawInputModifiers.None, PhysicalKey.Backspace, null)
+        window.KeyRelease(Key.Back, RawInputModifiers.None, PhysicalKey.Backspace, null)
+
+        // Type something
+        window.KeyTextInput "something"
+
+        Assert.IsNull(
+            calls |> Seq.last,
+            "Dynamic search should not be called with the activator anymore"
+        )
+
+        // Set another activator
+        window.KeyPress(Key.Back, RawInputModifiers.Control, PhysicalKey.Backspace, null)
+        window.KeyRelease(Key.Back, RawInputModifiers.Control, PhysicalKey.Backspace, null)
+        window.KeyTextInput activators[1].SearchEngineId
+
+        Assert.AreNotEqual(
+            activators[1],
+            calls |> Seq.last,
+            "Dynamic search engine should not be called"
+        )
+    )
